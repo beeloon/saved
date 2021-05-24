2a884652380cf93304047274a113b06f9876f5a4
Bohdan Bilyk <bohdanbilykwork@gmail.com>
2021-05-24 21:00:41 +0300

    add two new functions

---
+++ add-timeout-to-fn.js
@@ -0,0 +1,27 @@
+const timeout = (msec, fn) => {
+  let timer = setTimeout(() => {
+    if (timer) console.log("Function timedout");
+    timer = null;
+  }, msec);
+
+  return (...args) => {
+    if (timer) {
+      timer = null;
+      fn(...args);
+    }
+  };
+};
+
+const fn = (par) => {
+  console.log("Function called, par: " + par);
+};
+
+const fn100 = timeout(100, fn);
+const fn200 = timeout(200, fn);
+
+setTimeout(() => {
+  fn100("first");
+  fn200("second");
+}, 150);
+
+// first didn't
